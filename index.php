ce77f35e87fe28e45689c0e2f4a793288011180d
Timalm90 <timalm90@gmail.com>
2025-12-12 11:13:00 +0100

    Added base structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+require(__DIR__ . "/backend/vendor/autoload.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yrgopelago</title>
+    <link rel="stylesheet" href="frontend/styles/styles.css">
+
+
+</head>
+
+<body>
+
+    <scrpt src="/frontend/scripts/main.js"></scrpt>
+</body>
+
+</html>
